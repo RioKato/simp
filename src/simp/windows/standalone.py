b420e64a83087b74b2409fa1c43a54c5c3c0f636
rio <rio@kato.com>
2024-11-01 22:07:54 +0900

    update

--- src/simp/windows/standalone.py
+++ src/simp/windows/standalone.py
@@ -1,7 +1,8 @@
-from contextlib import contextmanager
+from contextlib import ExitStack, contextmanager
 from dataclasses import dataclass
+from msvcrt import get_osfhandle
+import os
 from subprocess import STARTF_USESTDHANDLES, STARTUPINFO, STD_INPUT_HANDLE, STD_OUTPUT_HANDLE, STD_ERROR_HANDLE, Popen
-from sys import stderr, stdin, stdout
 from typing import Iterator
 from .socketbridge import WinSocket
 from ..simp import Executor
@@ -11,28 +12,48 @@ from ..simp import Executor
 class Standalone(Executor[WinSocket]):
     @contextmanager
     def local(self, command: list[str], *, redirect: WinSocket | None = None, interactive: bool = False, tracable: bool = False, wait: bool = False) -> Iterator[int]:
-        if interactive:
-            hStdInput = redirect.fileno() if redirect else STD_INPUT_HANDLE
-            hStdOutput = redirect.fileno() if redirect else STD_OUTPUT_HANDLE
-            hStdError = redirect.fileno() if redirect else STD_ERROR_HANDLE
-        else:
-            hStdInput = redirect.fileno() if redirect else None
-            hStdOutput = redirect.fileno() if redirect else STD_OUTPUT_HANDLE
-            hStdError = redirect.fileno() if redirect else STD_ERROR_HANDLE
+        devnull = -1
 
-        startupinfo = STARTUPINFO(
-            dwFlags=STARTF_USESTDHANDLES,
-            hStdInput=hStdInput,
-            hStdOutput=hStdOutput,
-            hStdError=hStdError
-        )
-
-        with Popen(command, close_fds=False, startupinfo=startupinfo) as popen:
+        @contextmanager
+        def close_devnull() -> Iterator[None]:
             try:
-                yield popen.pid
+                yield
             finally:
-                if not wait and popen.poll() is None:
-                    popen.terminate()
+                if devnull >= 0:
+                    os.close(devnull)
+
+        with ExitStack() as estack:
+            estack.enter_context(close_devnull())
+
+            if interactive:
+                hStdInput = redirect.fileno() if redirect else STD_INPUT_HANDLE
+                hStdOutput = redirect.fileno() if redirect else STD_OUTPUT_HANDLE
+                hStdError = redirect.fileno() if redirect else STD_ERROR_HANDLE
+            else:
+                if redirect:
+                    hStdInput = redirect.fileno()
+                else:
+                    devnull = os.open(os.devnull, os.O_RDWR)
+                    hStdInput = get_osfhandle(devnull)
+
+                hStdOutput = redirect.fileno() if redirect else STD_OUTPUT_HANDLE
+                hStdError = redirect.fileno() if redirect else STD_ERROR_HANDLE
+
+            startupinfo = STARTUPINFO(
+                dwFlags=STARTF_USESTDHANDLES,
+                hStdInput=hStdInput,
+                hStdOutput=hStdOutput,
+                hStdError=hStdError
+            )
+
+            with Popen(command, close_fds=False, startupinfo=startupinfo) as popen:
+                estack.pop_all().close()
+
+                try:
+                    yield popen.pid
+                finally:
+                    if not wait and popen.poll() is None:
+                        popen.terminate()
 
     @contextmanager
     def remote(self, command: list[str], *, redirect: WinSocket | None = None, interactive: bool = False, tracable: bool = False, wait: bool = False) -> Iterator[int]:
